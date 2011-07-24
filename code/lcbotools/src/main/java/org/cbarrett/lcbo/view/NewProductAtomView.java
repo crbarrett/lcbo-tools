e0934c7a838f3b52fac2635eeb59ea5141d4b39d
Chris Barrett <christopher.r.barrett@gmail.com>
2011-07-24 13:30:58 -0400

    toying with names.

--- code/lcbotools/src/main/java/org/cbarrett/lcbo/view/NewProductAtomView.java
+++ code/lcbotools/src/main/java/org/cbarrett/lcbo/view/NewProductAtomView.java
@@ -38,7 +38,7 @@ public class NewProductAtomView extends AbstractAtomFeedView {
 
 	@Override
 	protected void buildFeedMetadata(Map<String, Object> model, Feed feed, HttpServletRequest request) {
-		feed.setId("tag:scotchbaby.com");
+		feed.setId("tag:lcbobaby.com");
 		feed.setTitle("New LCBO Product Listings");
 		@SuppressWarnings("unchecked")
 		List<Product> productList = (List<Product>) model.get("newProducts");
